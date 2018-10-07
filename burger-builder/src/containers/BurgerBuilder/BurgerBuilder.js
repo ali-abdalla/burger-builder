8157181caf6e0585ab5bb9e42a94ff2db6b76e6d
Ali Abdallah <aliabdallah93@gmail.com>
2018-10-07 14:28:17 +0200

    implemet add new ingredient

--- burger-builder/src/containers/BurgerBuilder/BurgerBuilder.js
+++ burger-builder/src/containers/BurgerBuilder/BurgerBuilder.js
@@ -3,6 +3,13 @@ import Aux from './../../hoc/Aux';
 import Burger from './../../components/Burger/Burger';
 import BuildControls from './../../components/Burger/BuildControls/BuildControls';
 
+const INGREDIENT_PRICES = {
+    salad: 0.5,
+    cheese: 0.5,
+    meat: 1.3,
+    bacon: 0.7
+};
+
 class BurgerBuilder extends Component {
 
     state = {
@@ -11,14 +18,23 @@ class BurgerBuilder extends Component {
             bacon: 0,
             cheese: 0,
             meat: 0
-        }
+        },
+        totalPrice: 4.0
+    }
+
+    addIngredientHandler = (type) => {
+        const updatedIngredients = {...this.state.ingredients};
+        updatedIngredients[type]++;
+        const updatedPrice = this.state.totalPrice + INGREDIENT_PRICES[type];
+
+        this.setState({totalPrice: updatedPrice, ingredients: updatedIngredients});
     }
 
     render() {
         return (
             <Aux>
                 <Burger ingredients={this.state.ingredients} />
-                <BuildControls />
+                <BuildControls ingredientAdded={this.addIngredientHandler} />
             </Aux>
         );
     }
